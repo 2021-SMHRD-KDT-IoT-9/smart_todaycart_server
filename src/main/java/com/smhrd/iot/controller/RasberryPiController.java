acced25be8304a88b24fcd8647bc13d3790e3996
user <user@59.0.236.228>
2023-05-25 16:31:50 +0900

    이미지 변환 컨트롤러,서비스 분리

--- src/main/java/com/smhrd/iot/controller/RasberryPiController.java
+++ src/main/java/com/smhrd/iot/controller/RasberryPiController.java
@@ -1,49 +1,26 @@
 package com.smhrd.iot.controller;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.spi.FileSystemProvider;
-import java.util.Arrays;
-import java.util.Base64;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartRequest;
 
-import com.smhrd.iot.domain.scanBarcode;
+import com.smhrd.iot.service.RaspberryPiService;
 
 @RestController
 public class RasberryPiController {
+	
+	@Autowired
+	RaspberryPiService service;
 
 	@PostMapping(value="/scan")
-	public void scanBarcode(@RequestParam("img") MultipartFile files) throws IOException{
-		System.out.println("작동");
-				
-		
-		byte [] imgData = files.getBytes();
-		String fileName = files.getOriginalFilename();
-		System.out.println("멀가?"+Arrays.toString(imgData));
-		System.out.println("원래이름"+fileName);
-		try (FileOutputStream fos = new FileOutputStream("C:\\Users\\user\\Desktop\\123.jpg")) {
-            fos.write(imgData);
-            fos.flush();
-            System.out.println("이미지 파일이 저장되었습니다.");
-        } catch (IOException e) {
-            System.out.println("이미지 파일 저장 중 오류가 발생했습니다: " + e.getMessage());
-        }
-		
-		
+	public void scanBarcode(@RequestParam("img") MultipartFile file) throws IOException{
+		service.dataToImage(file);
 	}
 	
 	@GetMapping(value="/barcode")
